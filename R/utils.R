b9a8e1a8bf1bc2449e9d8f1a6ef52ce862625a43
szcf-weiya <szcfweiya@gmail.com>
2025-03-18 17:43:07 -0400

    @new-csv: set base to master [skip ci]

--- R/utils.R
+++ R/utils.R
@@ -2,21 +2,21 @@ generate_new_csv_function = function(DataName, DataURL, DataY) {
   DataY = as.integer(DataY)
   sprintf('
 
-  ## ---- generate via generate_new_csv_function ---- ##
-  real_%s = function(prefix = "./real_data/") {
-    destfolder = file.path(prefix, "%s")
-    destfile = file.path(destfolder, "data.csv")
-    if (!file.exists(destfile)) {
-      if (!dir.exists(destfolder))
-        dir.create(destfolder, recursive = T)
-      download_with_retry("%s", destfile)
-    }
-    # assume no header
-    # header name is separate in this dataset repo: https://github.com/jbrownlee/Datasets
-    df = read.csv(destfile, header = FALSE)
-    list(x = model.matrix(~. -1, df[, -%d]),
-         y = df[,%d])
+## ---- generate via generate_new_csv_function ---- ##
+real_%s = function(prefix = "./real_data/") {
+  destfolder = file.path(prefix, "%s")
+  destfile = file.path(destfolder, "data.csv")
+  if (!file.exists(destfile)) {
+    if (!dir.exists(destfolder))
+      dir.create(destfolder, recursive = T)
+    download_with_retry("%s", destfile)
   }
+  # assume no header
+  # header name is separate in this dataset repo: https://github.com/jbrownlee/Datasets
+  df = read.csv(destfile, header = FALSE)
+  list(x = model.matrix(~. -1, df[, -%d]),
+       y = df[,%d])
+}
           ', DataName, DataName, DataURL, DataY, DataY)
 
 }
