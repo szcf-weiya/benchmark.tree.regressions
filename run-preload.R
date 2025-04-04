5aa12819ee41d21310cb430bbeb2b3de7d97c61f
szcf-weiya <szcfweiya@gmail.com>
2025-04-04 01:08:00 -0400

    save time by rm BART with ntree=200

--- run-preload.R
+++ run-preload.R
@@ -3,8 +3,8 @@ source("R/datasets.R")
 source("R/evaluate.R")
 source("R/methods.R")
 
-lst_methods = c(rep("bart_fit", 3),
-                rep("dbarts_fit", 3),
+lst_methods = c(rep("bart_fit", 2),
+                rep("dbarts_fit", 2),
                 rep("xbart_fit", 2),
                 rep("mars_fit", 4),
                 rep("ranger_fit", 3),
@@ -12,8 +12,8 @@ lst_methods = c(rep("bart_fit", 3),
                 rep("xgboost_fit", 1),
                 rep("carboost_fit", 1),
                 "mean_fit")
-names_lst_methods = c("BART_50", "BART_100", "BART_200",
-                      "dbarts_50", "dbarts_100", "dbarts_200",
+names_lst_methods = c("BART_50", "BART_100", #"BART_200",
+                      "dbarts_50", "dbarts_100", #"dbarts_200",
                       "XBART_10_40", "XBART_100_40", #"XBART_500_40", #"XBART_200_20", "XBART_200_80",
                        "MARS_d1", "MARS_d1_df", "MARS_d2", "MARS_d2_df",
                       "ranger_50", "ranger_100", "ranger_200",
@@ -24,8 +24,8 @@ names_lst_methods = c("BART_50", "BART_100", "BART_200",
                       "Baseline_mean")
 stopifnot(length(lst_methods) == length(names_lst_methods))
 names(lst_methods) = names_lst_methods
-lst_methods_paras = list(list(ntree = 50), list(ntree = 100), list(ntree = 200),
-                         list(ntree = 50), list(ntree = 100), list(ntree = 200),
+lst_methods_paras = list(list(ntree = 50), list(ntree = 100), #list(ntree = 200),
+                         list(ntree = 50), list(ntree = 100), #list(ntree = 200),
                          list(num_trees = 10, num_sweeps = 40, burnin = 15, mtry = "sqrtp"),
                          list(num_trees = 100, num_sweeps = 40, burnin = 15, mtry = "sqrtp"),
                          #list(num_trees = 500, num_sweeps = 40, burnin = 15),
